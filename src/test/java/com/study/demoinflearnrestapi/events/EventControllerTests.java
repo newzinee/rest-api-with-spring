85d135b2dca36d4324c61173ee354fcf10677695
treasureBear94 <qvo7896@gmail.com>
2020-12-28 23:18:52 +0900

    :sparkles: EventController - createEvent
    linkTo, ResponseEntity.created, objectMapper

--- src/test/java/com/study/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/study/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.study.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,7 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -21,11 +26,30 @@ class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Rest API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 12, 28,23,30,41))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 12, 29,23,30,41))
+                .beginEventDateTime(LocalDateTime.of(2020, 12, 30,23,30,41))
+                .endEventDateTime(LocalDateTime.of(2020, 12, 31,23,30,41))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
         mockMvc.perform(post("/api/events")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                    .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
