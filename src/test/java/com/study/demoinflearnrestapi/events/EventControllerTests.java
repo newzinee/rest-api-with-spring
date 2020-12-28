01d83df1234b6671e1c6311dd4386e2499f99422
treasureBear94 <qvo7896@gmail.com>
2020-12-28 22:54:05 +0900

    :white_check_mark: add create test

---
+++ src/test/java/com/study/demoinflearnrestapi/events/EventControllerTests.java
@@ -0,0 +1,33 @@
+package com.study.demoinflearnrestapi.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author yj
+ * @version 0.1.0
+ * @since 2020/12/28
+ */
+@SpringBootTest
+@WebMvcTest
+public class EventControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON))
+                .andExpect(status().isCreated());
+    }
+}
