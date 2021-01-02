24dd4a807d551f10688c33454679eb17bb477c79
treasureBear94 <qvo7896@gmail.com>
2021-01-03 01:24:28 +0900

    :recycle: withSelfRel 리팩토링

--- src/main/java/com/study/demoinflearnrestapi/events/EventResource.java
+++ src/main/java/com/study/demoinflearnrestapi/events/EventResource.java
@@ -3,6 +3,8 @@ package com.study.demoinflearnrestapi.events;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import org.springframework.hateoas.RepresentationModel;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 /**
  * @author yj
  * @version 0.1.0
@@ -15,6 +17,7 @@ public class EventResource extends RepresentationModel {
 
     public EventResource(Event event) {
         this.event = event;
+        add(linkTo(EventController.class).slash(event.getId()).withSelfRel());
     }
 
     public Event getEvent() {
